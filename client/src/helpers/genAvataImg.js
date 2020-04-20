f76d11b6f8065956448d29d84fcf1b4e4d5af2b6
Thang Truong <truongnt0279@gmail.com>
2020-04-20 17:51:25 +0700

    20200220 update displayname

--- client/src/helpers/genAvataImg.js
+++ client/src/helpers/genAvataImg.js
@@ -1,8 +1,6 @@
 export default function genAvataImg(name = 'user') {
   const avatarColors = ['#1C1C84', '#283258', '#1A6A8E', '#4AB1B5'];
-  const initial = name.match(/\b\w/g);
-
-  const displayName = `${initial[0]}${initial.length > 1 ? initial[initial.length - 1] : ''}`.toUpperCase();
+  const displayName = name.charAt(0) + '2';
   const backgroundColor = avatarColors[Math.floor(Math.random() * avatarColors.length)];
 
   return {
