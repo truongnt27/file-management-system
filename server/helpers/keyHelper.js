1dc076657c2a2c8cd60b774636be790437d0d669
Thang Truong <truongnt0279@gmail.com>
2020-05-29 09:25:57 +0700

    20200529 remove genKey to keyHelper

--- server/helpers/keyHelper.js
+++ server/helpers/keyHelper.js
@@ -1,8 +1,23 @@
+const crypto = require('crypto');
+
 const { ROTATE_PERIOD } = require('./constant');
 const convertDateToMilis = (str) => {
   const item = ROTATE_PERIOD.find(e => e.value === str);
   return item ? item.second : null;
 }
+
+
+const generateKey = () => {
+  const sharedSecret = crypto.randomBytes(32); // key_length = 256 bit
+  const textSecret = sharedSecret.toString('base64');
+
+  return {
+    plaintext: textSecret,
+    createdDate: Date.now()
+  }
+}
+
 module.exports = {
-  convertDateToMilis
+  convertDateToMilis,
+  generateKey
 }
